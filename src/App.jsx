b1e17272cc043ed22f2c307acf59acdbe80f28a6
Calet Devia <calebtius@gmail.com>
2025-06-14 09:01:09 -0500

    Created Footer structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,15 @@
 import TaskForm from "./components/Task/TaskForm";
 
+import Header from "./components/Layout/Header";
+
+import Footer from "./components/Layout/Footer";
+
 const App = () => {
   return (
     <>
+      <Header />
       <TaskForm />
+      <Footer />
     </>
   );
 };
---
+++ src/components/Layout/Footer.jsx
@@ -0,0 +1,11 @@
+const Footer = () => {
+  return (
+    <>
+      <footer>
+        <small> © 2025 - Calet Devia - Todos los Derechos Reservados</small>
+      </footer>
+    </>
+  );
+};
+
+export default Footer;
