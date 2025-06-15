c13c8cdacb0ff93e1886de83a5cb42e0987ad288
Calet Devia <calebtius@gmail.com>
2025-06-14 20:12:00 -0500

    Handled the input value in TaskForm

--- src/components/Task/TaskForm.jsx
+++ src/components/Task/TaskForm.jsx
@@ -2,14 +2,27 @@ import { useState } from "react";
 
 const TaskForm = ({ taskList, setTaskList }) => {
   const [task, setTask] = useState({
+    id: "",
     name: "",
     description: "",
     state: true,
   });
 
+  const { name } = task;
+
+  const handleForm = (e) => {
+    e.preventDefault();
+  };
+
+  const handleInput = (e) => {
+    const inputValue = e.target.value;
+
+    setTask({ ...task, name: inputValue });
+  };
+
   return (
-    <form action="">
-      <input type="text" name="" id="" />
+    <form action="" onSubmit={handleForm}>
+      <input onChange={handleInput} type="text" value={name} />
       <button>Enviar</button>
     </form>
   );
