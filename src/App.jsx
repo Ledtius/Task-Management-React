db63d5b6927502a72874b32340b6d50ccc050b23
Calet Devia <calebtius@gmail.com>
2025-06-17 10:08:13 -0500

    Created a state variable for TaskFilter the props were passed in TaskList and TaskFilter

--- src/App.jsx
+++ src/App.jsx
@@ -14,12 +14,18 @@ import Footer from "./components/Layout/Footer";
 const App = () => {
   const [taskList, setTaskList] = useState([]);
 
+  const [listState, setListState] = useState("all");
+
   return (
     <>
       <Header />
       <TaskForm taskList={taskList} setTaskList={setTaskList} />
-      <TaskFilter />
-      <TaskList taskList={taskList} setTaskList={setTaskList} />
+      <TaskFilter listState={listState} setListState={setListState} />
+      <TaskList
+        taskList={taskList}
+        setTaskList={setTaskList}
+        listState={listState}
+      />
       <Footer />
     </>
   );
