b851c176704baa7c8b6b81237bf174b31a7646ca
Calet Devia <calebtius@gmail.com>
2025-06-17 13:34:05 -0500

    Added '{}' on properties of the component for more props. Added new arrays to filter in based on the value of each button of TaskFilter and the state of each task of the taskList array of TaskList

--- src/components/Task/TaskList.jsx
+++ src/components/Task/TaskList.jsx
@@ -1,21 +1,90 @@
 import TaskItem from "./TaskItem";
 
-const TaskList = ({ taskList, setTaskList }) => {
-  return (
-    <ul>
-      {taskList.map(({ id, name, description, state }) => (
-        <TaskItem
-          idTask={id}
-          taskName={name}
-          taskDescrip={description}
-          taskState={state}
-          taskList={taskList}
-          setTaskList={setTaskList}
-          key={id}
-        />
-      ))}
-    </ul>
-  );
+const TaskList = ({ taskList, setTaskList, listState }) => {
+  console.log(listState);
+  if (listState === "all") {
+    return (
+      <ul>
+        {taskList.map(({ id, name, description, state }) => (
+          <TaskItem
+            idTask={id}
+            taskName={name}
+            taskDescrip={description}
+            taskState={state}
+            taskList={taskList}
+            setTaskList={setTaskList}
+            key={id}
+          />
+        ))}
+      </ul>
+    );
+  } else if (listState === "incomplete") {
+    const taskListIncomplete = taskList.filter((task) => {
+      if (task.state === false) return task;
+    });
+    return (
+      <ul>
+        {taskListIncomplete.map(({ id, name, description, state }) => {
+          return (
+            <TaskItem
+              idTask={id}
+              taskName={name}
+              taskDescrip={description}
+              taskState={state}
+              taskList={taskList}
+              setTaskList={setTaskList}
+              key={id}
+            />
+          );
+        })}
+      </ul>
+    );
+  } else {
+    const taskListComplete = taskList.filter((task) => {
+      if (task.state === true) return task;
+    });
+
+    return (
+      <ul>
+        {taskListComplete.map(({ id, name, description, state }) => {
+          if (state === true)
+            return (
+              <TaskItem
+                idTask={id}
+                taskName={name}
+                taskDescrip={description}
+                taskState={state}
+                taskList={taskList}
+                setTaskList={setTaskList}
+                key={id}
+              />
+            );
+        })}
+      </ul>
+    );
+  }
 };
+
+/* Why this doesn't work?
+
+ return (
+      <ul>
+        {taskList.filter(({ id, name, description, state }) => {
+          if (state === true)
+            return (
+              <TaskItem
+                idTask={id}
+                taskName={name}
+                taskDescrip={description}
+                taskState={state}
+                taskList={taskList}
+                setTaskList={setTaskList}
+                key={id}
+              />
+            );
+        })}
+      </ul>
+    );
+*/
 
 export default TaskList;
