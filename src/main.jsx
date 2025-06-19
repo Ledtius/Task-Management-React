e06cdff459fb4249c3d41a0947edf0d22946b4d8
Calet Devia <calebtius@gmail.com>
2025-06-19 15:16:38 -0500

    Exported the based styles

--- src/main.jsx
+++ src/main.jsx
@@ -1,5 +1,8 @@
 import { createRoot } from "react-dom/client";
 
+import "./styles/general.css";
+import "./styles/variables.css";
+
 import App from "./App";
 
 createRoot(document.getElementById("root")).render(
