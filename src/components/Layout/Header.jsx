3011a2b157e9a1086999853ef2b2767925b6992a
Calet Devia <calebtius@gmail.com>
2025-06-21 10:42:58 -0500

    Added styles to the Header

--- src/components/Layout/Header.jsx
+++ src/components/Layout/Header.jsx
@@ -1,8 +1,10 @@
+import headerStyles from "../../styles/Layout/Header.module.css";
+
 const Header = () => {
   return (
     <>
       <header>
-        <h1>Gestor de tareas</h1>
+        <h1 className={headerStyles.h1}>Gestor de tareas</h1>
       </header>
     </>
   );
