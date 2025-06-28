5eda3cfba27f2fd4c883e51eb2d0031cee687168
Calet Devia <calebtius@gmail.com>
2025-06-28 11:14:06 -0500

    Deleted the  taskListFilterByName function by taskListFilter (A complete function)

--- src/components/Task/TaskList.jsx
+++ src/components/Task/TaskList.jsx
@@ -4,142 +4,43 @@ import taskListStyles from "../../styles/Task/TaskList.module.css";
 const TaskList = ({ taskList, setTaskList, listState, filterInput }) => {
   console.log(listState);
 
-  const taskListFilterByName = () => {
-    const taskListByName = taskList.filter(({ name }) =>
-      name.toLowerCase().includes(filterInput.toLowerCase())
-    );
-    return taskListByName;
+  const taskListFilter = () => {
+    let translateState;
+
+    if (listState === "incomplete") translateState = false;
+    else if (listState === "complete") translateState = true;
+
+    if (listState === "incomplete" || listState === "complete")
+      return taskList
+        .filter(({ state }) => state === translateState)
+        .filter(({ name }) =>
+          name.toLowerCase().includes(filterInput.toLowerCase())
+        );
+    else
+      return taskList.filter(({ name }) =>
+        name.toLowerCase().includes(filterInput.toLowerCase())
+      );
   };
 
-  console.log(taskListFilterByName());
+  console.log(taskListFilter());
 
-  if (listState === "all") {
-    if (!filterInput) {
-      return (
-        <ul className={taskListStyles.list}>
-          {taskList.map(({ id, name, description, state }) => (
-            <TaskItem
-              idTask={id}
-              taskName={name}
-              taskDescrip={description}
-              taskState={state}
-              taskList={taskList}
-              setTaskList={setTaskList}
-              key={id}
-            />
-          ))}
-        </ul>
-      );
-    } else {
-      const taskListFilterAll = taskList.filter(({ name }) =>
-        name.toLowerCase().includes(filterInput.toLowerCase())
-      );
-
-      return (
-        <ul className={taskListStyles.list}>
-          {taskListFilterAll.map(({ id, name, description, state }) => (
-            <TaskItem
-              idTask={id}
-              taskName={name}
-              taskDescrip={description}
-              taskState={state}
-              taskList={taskList}
-              setTaskList={setTaskList}
-              key={id}
-            />
-          ))}
-        </ul>
-      );
-    }
-  } else if (listState === "incomplete") {
-    const taskListIncomplete = taskList.filter((task) => task.state === false);
-    if (!filterInput) {
-      return (
-        <ul className={taskListStyles.list}>
-          {taskListIncomplete.map(({ id, name, description, state }) => {
-            return (
-              <TaskItem
-                idTask={id}
-                taskName={name}
-                taskDescrip={description}
-                taskState={state}
-                taskList={taskList}
-                setTaskList={setTaskList}
-                key={id}
-              />
-            );
-          })}
-        </ul>
-      );
-    } else {
-      const taskListIncompleteFilter = taskListIncomplete.filter(({ name }) =>
-        name.toLowerCase().includes(filterInput.toLowerCase())
-      );
-
-      return (
-        <ul className={taskListStyles.list}>
-          {taskListIncompleteFilter.map(({ id, name, description, state }) => {
-            return (
-              <TaskItem
-                idTask={id}
-                taskName={name}
-                taskDescrip={description}
-                taskState={state}
-                taskList={taskList}
-                setTaskList={setTaskList}
-                key={id}
-              />
-            );
-          })}
-        </ul>
-      );
-    }
-  } else {
-    const taskListComplete = taskList.filter((task) => task.state === true);
-    if (!filterInput) {
-      return (
-        <ul className={taskListStyles.list}>
-          {taskListComplete.map(({ id, name, description, state }) => {
-            if (state === true)
-              return (
-                <TaskItem
-                  idTask={id}
-                  taskName={name}
-                  taskDescrip={description}
-                  taskState={state}
-                  taskList={taskList}
-                  setTaskList={setTaskList}
-                  key={id}
-                />
-              );
-          })}
-        </ul>
-      );
-    } else {
-      const taskListCompleteFilter = taskListComplete.filter(({ name }) =>
-        name.toLowerCase().includes(filterInput.toLowerCase())
-      );
-
-      return (
-        <ul className={taskListStyles.list}>
-          {taskListCompleteFilter.map(({ id, name, description, state }) => {
-            if (state === true)
-              return (
-                <TaskItem
-                  idTask={id}
-                  taskName={name}
-                  taskDescrip={description}
-                  taskState={state}
-                  taskList={taskList}
-                  setTaskList={setTaskList}
-                  key={id}
-                />
-              );
-          })}
-        </ul>
-      );
-    }
-  }
+  return (
+    <ul className={taskListStyles.list}>
+      {taskListFilter().map(({ id, name, description, state }) => {
+        return (
+          <TaskItem
+            idTask={id}
+            taskName={name}
+            taskDescrip={description}
+            taskState={state}
+            taskList={taskList}
+            setTaskList={setTaskList}
+            key={id}
+          />
+        );
+      })}
+    </ul>
+  );
 };
 
 export default TaskList;
