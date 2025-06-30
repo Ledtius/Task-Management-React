7d2fd2c172eb0be1ccd80eb4a16aa817b67ae14f
Calet Devia <calebtius@gmail.com>
2025-06-29 21:01:05 -0500

    Separated the taskDescrip logic from other component and fixed a lot of useState variables

---
+++ src/components/Task/TaskDescription.jsx
@@ -0,0 +1,317 @@
+import { useEffect, useState } from "react";
+import { LucideEdit, CirclePlus, Trash2, Save, Ban } from "lucide-react";
+
+import taskItemStyles from "../../styles/Task/taskItem.module.css";
+
+const TaskDescription = ({
+  taskList,
+  setTaskList,
+  taskDescrip,
+  idTask,
+  taskName,
+}) => {
+  const [descripInput, setDescripInput] = useState(taskDescrip);
+
+  const [editDescrip, setEditDescrip] = useState("");
+
+  const [taskDescripShow, setTaskDescripShow] = useState({
+    showAddDescripSign: true,
+    showAddDescripPanel: false,
+    showAlertAddDescripPanel: false,
+    showDescrip: false,
+    showEditDescripPanel: false,
+    showAlertEditDescripPanel: false,
+  });
+
+  const {
+    showAddDescripSign,
+    showAddDescripPanel,
+    showAlertAddDescripPanel,
+    showDescrip,
+    showEditDescripPanel,
+    showAlertEditDescripPanel,
+  } = taskDescripShow;
+
+  useEffect(() => {
+    taskDescrip
+      ? setTaskDescripShow({
+          ...taskDescripShow,
+          showDescrip: true,
+          showAddDescripSign: false,
+        })
+      : setTaskDescripShow({
+          ...taskDescripShow,
+          showDescrip: false,
+          showAddDescripSign: true,
+        });
+  }, [taskList]);
+
+  const handleAddDescripBtn = () => {
+    setTaskDescripShow({
+      ...taskDescripShow,
+      showAddDescripSign: false,
+      showAddDescripPanel: true,
+    });
+
+    // setShowAddDescrip(true);
+    // setShowAddDescripBtn(false);
+  };
+
+  const handleAddDescripInput = (e) => {
+    const inputValue = e.target.value;
+
+    setDescripInput(inputValue);
+  };
+
+  const handleAddDescripAddBtn = (e) => {
+    console.log(taskDescrip);
+    console.log(descripInput);
+    if (descripInput) {
+      setTaskDescripShow({
+        ...taskDescripShow,
+        showAddDescripPanel: false,
+        showDescrip: true,
+      });
+      // setShowAddDescripBtn(false);
+      // setShowAddDescrip(false);
+      // setShowDescripMsg(true);
+      setTaskList((prevTaskList) => {
+        return prevTaskList.map((task) => {
+          if (task.name === taskName) {
+            return { ...task, description: descripInput };
+          }
+          return task;
+        });
+      });
+    } else {
+      setTimeout(() => {
+        setTaskDescripShow({
+          ...taskDescripShow,
+          showAlertAddDescripPanel: false,
+        });
+        // setShowAlertAddDescrip(false);
+      }, 1500);
+      setTaskDescripShow({
+        ...taskDescripShow,
+        showAlertAddDescripPanel: true,
+      });
+
+      // setShowAlertAddDescrip(true);
+    }
+  };
+
+  const handleAddDescripCancelBtn = () => {
+    setTaskDescripShow({
+      ...taskDescripShow,
+      showAddDescripPanel: false,
+      showAddDescripSign: true,
+    });
+    // setShowAddDescrip(false);
+    // setShowAddDescripBtn(true);
+    setDescripInput("");
+  };
+
+  const handleEditDescripBtn = () => {
+    setTaskDescripShow({
+      ...taskDescripShow,
+      showEditDescripPanel: true,
+      showDescrip: false,
+    });
+    // setShowEditDescrip(true);
+    // setShowDescripMsg(false);
+  };
+
+  const handleEditDescripInput = (e) => {
+    const inputValue = e.target.value;
+
+    setEditDescrip(inputValue);
+  };
+
+  const handleEditDescripAcceptBtn = () => {
+    console.log(editDescrip);
+    if (editDescrip) {
+      setTaskDescripShow({
+        ...taskDescripShow,
+        showEditDescripPanel: false,
+        showDescrip: true,
+      });
+      // setShowEditDescrip(false);
+      // setShowDescripMsg(true);
+      setTaskList((prevTaskList) => {
+        return prevTaskList.map((task) => {
+          if (task.description === taskDescrip) {
+            return { ...task, description: editDescrip };
+          }
+          return task;
+        });
+      });
+    } else {
+      setTimeout(() => {
+        setTaskDescripShow({
+          ...taskDescripShow,
+          showAlertEditDescripPanel: false,
+        });
+        // setShowAlertEditDescrip(false);
+      }, 1500);
+      // setShowAlertEditDescrip(true);
+      setTaskDescripShow({
+        ...taskDescripShow,
+        showAlertEditDescripPanel: true,
+      });
+    }
+  };
+
+  const handleEditDescripCancelBtn = () => {
+    setTaskDescripShow({
+      ...taskDescripShow,
+      showEditDescripPanel: false,
+      showDescrip: true,
+    });
+
+    // setShowEditDescrip(false);
+    // setShowDescripMsg(true);
+    setEditDescrip(taskDescrip);
+  };
+
+  const handleEditDescripDeleteBtn = () => {
+    // setShowAddDescrip(true);
+    setDescripInput("");
+
+    setTaskDescripShow({
+      ...taskDescripShow,
+      showAddDescripSign: true,
+      showDescrip: false,
+    });
+
+    // setShowAddDescripBtn(true);
+    // setShowDescripMsg(false);
+    setTaskList((prevTaskList) => {
+      return prevTaskList.map((task) => {
+        if (task.id === idTask) {
+          return { ...task, description: "" };
+        }
+        return task;
+      });
+    });
+  };
+  return (
+    <>
+      {/* Añadir descripción */}
+      {showAddDescripSign && (
+        <footer
+          className={taskItemStyles.footer}
+          onClick={handleAddDescripBtn}
+          title="Añadir descripción de la tarea"
+        >
+          <small className={taskItemStyles.descripMsg}>
+            Añadir descripción
+          </small>
+          <button className={taskItemStyles.addDescripBtn}>
+            <CirclePlus className={taskItemStyles.addDescripIcon} />
+          </button>
+        </footer>
+      )}
+      {/* Panel para escribir la descripción */}
+      {showAddDescripPanel && (
+        <div className={taskItemStyles.editAddPanel}>
+          <input
+            className={taskItemStyles.editAddPanelInput}
+            type="text"
+            placeholder="Descripción de la tarea"
+            onChange={handleAddDescripInput}
+            value={descripInput}
+          />
+
+          {showAlertAddDescripPanel && (
+            <div className={taskItemStyles.editAddPanelAlertSection}>
+              <small className={taskItemStyles.editAddPanelAlert}>
+                Debes de ingresar una descripción valida
+              </small>
+            </div>
+          )}
+          <nav className={taskItemStyles.editAddPanelAC}>
+            <button
+              className={taskItemStyles.editAddPanelAccept}
+              onClick={handleAddDescripAddBtn}
+            >
+              <CirclePlus
+                className={taskItemStyles.editAddPanelAcceptBtnIcon}
+              />
+              Añadir
+            </button>
+            <button
+              className={taskItemStyles.editAddPanelCancel}
+              onClick={handleAddDescripCancelBtn}
+            >
+              <Ban className={taskItemStyles.editAddPanelCancelBtnIcon} />
+              Cancelar
+            </button>
+          </nav>
+        </div>
+      )}
+
+      {/* Nombre de la descripción  */}
+      {showDescrip && (
+        <div className={taskItemStyles.taskDescripSection}>
+          <small className={taskItemStyles.taskDescrip} title="Descripción">
+            {taskDescrip}
+          </small>
+          <nav className={taskItemStyles.taskDescripEdDe}>
+            <button
+              className={taskItemStyles.taskDescripEdit}
+              onClick={handleEditDescripBtn}
+              title="Editar descripción"
+            >
+              <LucideEdit className={taskItemStyles.taskDescripEditBtnIcon} />
+            </button>
+
+            <button
+              className={taskItemStyles.taskDescripDelete}
+              onClick={handleEditDescripDeleteBtn}
+              title="Borrar descripción"
+            >
+              <Trash2 className={taskItemStyles.taskDescripDeleteBtnIcon} />
+            </button>
+          </nav>
+        </div>
+      )}
+      {/* Panel cuando das a editar */}
+      {showEditDescripPanel && (
+        <div className={taskItemStyles.editAddPanel}>
+          <input
+            className={taskItemStyles.editAddPanelInput}
+            type="text"
+            placeholder={"Editando descripción..."}
+            value={editDescrip}
+            onChange={handleEditDescripInput}
+          />
+          {showAlertEditDescripPanel && (
+            <div className={taskItemStyles.editAddPanelAlertSection}>
+              <small className={taskItemStyles.editAddPanelAlert}>
+                Debes de ingresar una descripción valida
+              </small>
+            </div>
+          )}
+          <nav className={taskItemStyles.editAddPanelAC}>
+            <button
+              className={taskItemStyles.editAddPanelAccept}
+              onClick={handleEditDescripAcceptBtn}
+            >
+              <Save className={taskItemStyles.editAddPanelAcceptBtnIcon} />
+              Guardar
+            </button>
+            <button
+              className={taskItemStyles.editAddPanelCancel}
+              onClick={handleEditDescripCancelBtn}
+            >
+              <Ban className={taskItemStyles.editAddPanelCancelBtnIcon} />
+              Cancelar
+            </button>
+          </nav>
+        </div>
+      )}
+    </>
+  );
+};
+
+export default TaskDescription;
--- src/components/Task/TaskItem.jsx
+++ src/components/Task/TaskItem.jsx
@@ -2,6 +2,7 @@ import { useState } from "react";
 import { LucideEdit, CirclePlus, Trash2, Save, Ban } from "lucide-react";
 
 import TaskName from "./TaskName";
+import TaskDescription from "./TaskDescription";
 
 import taskItemStyles from "../../styles/Task/TaskItem.module.css";
 
@@ -15,123 +16,123 @@ const TaskItem = ({
 }) => {
   /* Description state variables */
 
-  const [descripInput, setDescripInput] = useState(taskDescrip);
+  // const [descripInput, setDescripInput] = useState(taskDescrip);
 
-  const [editDescrip, setEditDescrip] = useState("");
+  // const [editDescrip, setEditDescrip] = useState("");
 
-  const [showAddDecrip, setShowAddDescrip] = useState(false);
+  // const [showAddDecrip, setShowAddDescrip] = useState(false);
 
-  const [showAddDescripBtn, setShowAddDescripBtn] = useState(() => {
-    if (taskDescrip) return false;
-    return true;
-  });
+  // const [showAddDescripBtn, setShowAddDescripBtn] = useState(() => {
+  //   if (taskDescrip) return false;
+  //   return true;
+  // });
 
-  const [showDescripMsg, setShowDescripMsg] = useState(() => {
-    if (taskDescrip) return true;
-    return false;
-  });
+  // const [showDescripMsg, setShowDescripMsg] = useState(() => {
+  //   if (taskDescrip) return true;
+  //   return false;
+  // });
 
-  const [showEditDescrip, setShowEditDescrip] = useState(false);
+  // const [showEditDescrip, setShowEditDescrip] = useState(false);
 
-  const [showAlertAddDescrip, setShowAlertAddDescrip] = useState(false);
+  // const [showAlertAddDescrip, setShowAlertAddDescrip] = useState(false);
 
-  const [showAlertEditDescrip, setShowAlertEditDescrip] = useState(false);
+  // const [showAlertEditDescrip, setShowAlertEditDescrip] = useState(false);
 
   /* Description functions section */
 
-  const handleAddDescripBtn = () => {
-    setShowAddDescrip(true);
-    setShowAddDescripBtn(false);
-  };
+  // const handleAddDescripBtn = () => {
+  //   setShowAddDescrip(true);
+  //   setShowAddDescripBtn(false);
+  // };
 
-  const handleAddDescripInput = (e) => {
-    const inputValue = e.target.value;
+  // const handleAddDescripInput = (e) => {
+  //   const inputValue = e.target.value;
 
-    setDescripInput(inputValue);
-  };
+  //   setDescripInput(inputValue);
+  // };
 
-  const handleAddDescripAddBtn = (e) => {
-    console.log(taskDescrip);
-    if (descripInput) {
-      setShowAddDescripBtn(false);
-      setShowAddDescrip(false);
-      setShowDescripMsg(true);
-      setTaskList((prevTaskList) => {
-        return prevTaskList.map((task) => {
-          if (task.name === taskName) {
-            return { ...task, description: descripInput };
-          }
-          return task;
-        });
-      });
-    } else {
-      setTimeout(() => {
-        setShowAlertAddDescrip(false);
-      }, 1500);
-      setShowAlertAddDescrip(true);
-    }
-  };
+  // const handleAddDescripAddBtn = (e) => {
+  //   console.log(taskDescrip);
+  //   if (descripInput) {
+  //     setShowAddDescripBtn(false);
+  //     setShowAddDescrip(false);
+  //     setShowDescripMsg(true);
+  //     setTaskList((prevTaskList) => {
+  //       return prevTaskList.map((task) => {
+  //         if (task.name === taskName) {
+  //           return { ...task, description: descripInput };
+  //         }
+  //         return task;
+  //       });
+  //     });
+  //   } else {
+  //     setTimeout(() => {
+  //       setShowAlertAddDescrip(false);
+  //     }, 1500);
+  //     setShowAlertAddDescrip(true);
+  //   }
+  // };
 
-  const handleAddDescripCancelBtn = () => {
-    setShowAddDescrip(false);
-    setShowAddDescripBtn(true);
-    setDescripInput("");
-  };
+  // const handleAddDescripCancelBtn = () => {
+  //   setShowAddDescrip(false);
+  //   setShowAddDescripBtn(true);
+  //   setDescripInput("");
+  // };
 
-  const handleEditDescripBtn = () => {
-    setShowEditDescrip(true);
-    setShowDescripMsg(false);
-  };
+  // const handleEditDescripBtn = () => {
+  //   setShowEditDescrip(true);
+  //   setShowDescripMsg(false);
+  // };
 
-  const handleEditDescripInput = (e) => {
-    const inputValue = e.target.value;
+  // const handleEditDescripInput = (e) => {
+  //   const inputValue = e.target.value;
 
-    setEditDescrip(inputValue);
-  };
+  //   setEditDescrip(inputValue);
+  // };
 
-  const handleEditDescripAcceptBtn = () => {
-    console.log(editDescrip);
-    if (editDescrip) {
-      setShowEditDescrip(false);
-      setShowDescripMsg(true);
-      setTaskList((prevTaskList) => {
-        return prevTaskList.map((task) => {
-          if (task.description === taskDescrip) {
-            return { ...task, description: editDescrip };
-          }
-          return task;
-        });
-      });
-    } else {
-      console.log("12");
-      setTimeout(() => {
-        setShowAlertEditDescrip(false);
-      }, 1500);
-      setShowAlertEditDescrip(true);
-    }
-  };
+  // const handleEditDescripAcceptBtn = () => {
+  //   console.log(editDescrip);
+  //   if (editDescrip) {
+  //     setShowEditDescrip(false);
+  //     setShowDescripMsg(true);
+  //     setTaskList((prevTaskList) => {
+  //       return prevTaskList.map((task) => {
+  //         if (task.description === taskDescrip) {
+  //           return { ...task, description: editDescrip };
+  //         }
+  //         return task;
+  //       });
+  //     });
+  //   } else {
+  //     console.log("12");
+  //     setTimeout(() => {
+  //       setShowAlertEditDescrip(false);
+  //     }, 1500);
+  //     setShowAlertEditDescrip(true);
+  //   }
+  // };
 
-  const handleEditDescripCancelBtn = () => {
-    setShowEditDescrip(false);
-    setShowDescripMsg(true);
-    setEditDescrip(taskDescrip);
-  };
+  // const handleEditDescripCancelBtn = () => {
+  //   setShowEditDescrip(false);
+  //   setShowDescripMsg(true);
+  //   setEditDescrip(taskDescrip);
+  // };
 
-  const handleEditDescripDeleteBtn = () => {
-    // setShowAddDescrip(true);
-    setDescripInput("");
-    setShowAddDescripBtn(true);
+  // const handleEditDescripDeleteBtn = () => {
+  //   // setShowAddDescrip(true);
+  //   setDescripInput("");
+  //   setShowAddDescripBtn(true);
 
-    setShowDescripMsg(false);
-    setTaskList((prevTaskList) => {
-      return prevTaskList.map((task) => {
-        if (task.id === idTask) {
-          return { ...task, description: "" };
-        }
-        return task;
-      });
-    });
-  };
+  //   setShowDescripMsg(false);
+  //   setTaskList((prevTaskList) => {
+  //     return prevTaskList.map((task) => {
+  //       if (task.id === idTask) {
+  //         return { ...task, description: "" };
+  //       }
+  //       return task;
+  //     });
+  //   });
+  // };
 
   /* Checkbox functions section */
 
@@ -184,122 +185,15 @@ const TaskItem = ({
             setTaskList={setTaskList}
             taskState={taskState}
           />
-          {/* Añadir descripción */}
-          {showAddDescripBtn && (
-            <footer
-              className={taskItemStyles.footer}
-              onClick={handleAddDescripBtn}
-              title="Añadir descripción de la tarea"
-            >
-              <small className={taskItemStyles.descripMsg}>
-                Añadir descripción
-              </small>
-              <button className={taskItemStyles.addDescripBtn}>
-                <CirclePlus className={taskItemStyles.addDescripIcon} />
-              </button>
-            </footer>
-          )}
-          {/* Panel para escribir la descripción */}
-          {showAddDecrip && (
-            <div className={taskItemStyles.editAddPanel}>
-              <input
-                className={taskItemStyles.editAddPanelInput}
-                type="text"
-                placeholder="Descripción de la tarea"
-                onChange={handleAddDescripInput}
-                value={descripInput}
-              />
-
-              {showAlertAddDescrip && (
-                <div className={taskItemStyles.editAddPanelAlertSection}>
-                  <small className={taskItemStyles.editAddPanelAlert}>
-                    Debes de ingresar una descripción valida
-                  </small>
-                </div>
-              )}
-              <nav className={taskItemStyles.editAddPanelAC}>
-                <button
-                  className={taskItemStyles.editAddPanelAccept}
-                  onClick={handleAddDescripAddBtn}
-                >
-                  <CirclePlus
-                    className={taskItemStyles.editAddPanelAcceptBtnIcon}
-                  />
-                  Añadir
-                </button>
-                <button
-                  className={taskItemStyles.editAddPanelCancel}
-                  onClick={handleAddDescripCancelBtn}
-                >
-                  <Ban className={taskItemStyles.editAddPanelCancelBtnIcon} />
-                  Cancelar
-                </button>
-              </nav>
-            </div>
-          )}
-          {/* Cuando se añada la descripción */}
-          {showDescripMsg && (
-            <div className={taskItemStyles.taskDescripSection}>
-              <small className={taskItemStyles.taskDescrip} title="Descripción">
-                {taskDescrip}
-              </small>
-              <nav className={taskItemStyles.taskDescripEdDe}>
-                <button
-                  className={taskItemStyles.taskDescripEdit}
-                  onClick={handleEditDescripBtn}
-                  title="Editar descripción"
-                >
-                  <LucideEdit
-                    className={taskItemStyles.taskDescripEditBtnIcon}
-                  />
-                </button>
-
-                <button
-                  className={taskItemStyles.taskDescripDelete}
-                  onClick={handleEditDescripDeleteBtn}
-                  title="Borrar descripción"
-                >
-                  <Trash2 className={taskItemStyles.taskDescripDeleteBtnIcon} />
-                </button>
-              </nav>
-            </div>
-          )}
-          {/* Panel cuando das a editar */}
-          {showEditDescrip && (
-            <div className={taskItemStyles.editAddPanel}>
-              <input
-                className={taskItemStyles.editAddPanelInput}
-                type="text"
-                placeholder={"Editando descripción..."}
-                value={editDescrip}
-                onChange={handleEditDescripInput}
-              />
-              {showAlertEditDescrip && (
-                <div className={taskItemStyles.editAddPanelAlertSection}>
-                  <small className={taskItemStyles.editAddPanelAlert}>
-                    Debes de ingresar una descripción valida
-                  </small>
-                </div>
-              )}
-              <nav className={taskItemStyles.editAddPanelAC}>
-                <button
-                  className={taskItemStyles.editAddPanelAccept}
-                  onClick={handleEditDescripAcceptBtn}
-                >
-                  <Save className={taskItemStyles.editAddPanelAcceptBtnIcon} />
-                  Guardar
-                </button>
-                <button
-                  className={taskItemStyles.editAddPanelCancel}
-                  onClick={handleEditDescripCancelBtn}
-                >
-                  <Ban className={taskItemStyles.editAddPanelCancelBtnIcon} />
-                  Cancelar
-                </button>
-              </nav>
-            </div>
-          )}
+          <TaskDescription
+            taskList={taskList}
+            setTaskList={setTaskList}
+            taskDescrip={taskDescrip}
+            taskName={taskName}
+            idTask={idTask}
+          />
         </section>
+
         <button
           className={taskItemStyles.deleteBtn}
           onClick={handleTaskDeleteBtn}
