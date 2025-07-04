5bfaa63b1452a19b9d3e945d4b7b86e70d89fdf3
Calet Devia <calebtius@gmail.com>
2025-07-04 15:44:05 -0500

    Added a TaskPriority component

--- src/components/Task/TaskPriority.jsx
+++ src/components/Task/TaskPriority.jsx
@@ -1,14 +1,14 @@
-import { LucideStar, LucideDiamondPlus } from "lucide-react";
+import { Settings2, LucideDiamondPlus } from "lucide-react";
 import { useEffect, useState } from "react";
 import taskPriorityStyles from "../../styles/Task/TaskPriority.module.css";
 
-const TaskPriority = () => {
+const TaskPriority = ({ setTaskList, idTask, taskPriority }) => {
   const [priorityStyles, setPriorityStyles] = useState({
     showMessage: true,
     showPriority: false,
   });
 
-  const [priorityValue, setPriorityValue] = useState("");
+  const [priorityValue, setPriorityValue] = useState("low");
 
   const { showMessage, showPriority } = priorityStyles;
 
@@ -17,51 +17,27 @@ const TaskPriority = () => {
   };
 
   useEffect(() => {
-    console.log(priorityValue);
+    if (priorityValue !== "low")
+      setTaskList((prevTaskList) =>
+        prevTaskList.map((task) => {
+          if (task.id === idTask) return { ...task, priority: priorityValue };
+          return task;
+        })
+      );
   }, [priorityValue]);
 
-  const handleStarValue = (e) => {
-    const valueId = e.currentTarget.id;
-
+  const handleSelect = (e) => {
+    const valueId = e.currentTarget.value;
     setPriorityValue(valueId);
   };
   return (
-    <section className={taskPriorityStyles.prioritySection}>
-      {showMessage && (
-        <div className={taskPriorityStyles.btnMsg} onClick={handleMessage}>
-          <small className={taskPriorityStyles.msg}>Añadir prioridad</small>
-          <button className={taskPriorityStyles.btn}>
-            <LucideDiamondPlus className={taskPriorityStyles.Addicon} />
-          </button>
-        </div>
-      )}
-      {showPriority && (
-        <nav className={taskPriorityStyles.priorityNav}>
-          <button
-            id="1"
-            className={taskPriorityStyles.starBtn}
-            onClick={handleStarValue}
-          >
-            <LucideStar className={taskPriorityStyles.starIcon} />
-            <small className={taskPriorityStyles.starValue}>1</small>
-          </button>
-          <button
-            className={taskPriorityStyles.starBtn}
-            id="2"
-            onClick={handleStarValue}
-          >
-            <LucideStar className={taskPriorityStyles.starIcon} />
-          </button>
-          <button
-            className={taskPriorityStyles.starBtn}
-            id="3"
-            onClick={handleStarValue}
-          >
-            <LucideStar className={taskPriorityStyles.starIcon} />
-          </button>
-        </nav>
-      )}{" "}
-    </section>
+    <form>
+      <select onClick={handleSelect} name="" id="">
+        <option value="low">Baja</option>
+        <option value="medium">Media</option>
+        <option value="hight">Alta</option>
+      </select>
+    </form>
   );
 };
 
